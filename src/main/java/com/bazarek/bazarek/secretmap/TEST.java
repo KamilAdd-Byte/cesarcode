fc7ca488d0aa5156dec30d99f24d2b48f22ffa38
KamilAdd-Byte <kamillodzinski09@gmail.com>
2022-03-26 20:01:29 +0100

    refactor(map): Changed movie types and added method for UserKamilSecretMap.

--- src/main/java/com/bazarek/bazarek/secretmap/TEST.java
+++ src/main/java/com/bazarek/bazarek/secretmap/TEST.java
@@ -1,8 +1,12 @@
 package com.bazarek.bazarek.secretmap;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class TEST {
     public static void main(String[] args) {
@@ -11,7 +15,7 @@ public class TEST {
         //dodanie filmu
         int size = u.myMoviesToMap().size();
         System.out.println("Ilość filmów: " + size);
-        u.addMovieToMap("WWW",new Movies("Nowt",2021,6.99,"Poland",new Movies.TypeMovies("Horror",false)));
+        u.addMovieToMap("WWW",new Movies("Nowt",2021,6.99,"Poland",new Movies.TypeMovies(Movies.Category.HORROR,false)));
         int size2 = u.myMoviesToMap().size();
         System.out.println(size2);
        //klucze
@@ -32,7 +36,19 @@ public class TEST {
                 .filter(movies -> movies.getRating() > 5)
                 .filter(m -> m.getCountry().equalsIgnoreCase("Usa"))
                 .forEach(System.out::println);
-        }
+
+
+        StringBuilder batman = u.test("Batman", "Batman-Mroczny rycerz");
+        System.out.println(batman);
+
+    int count = Stream.of(1,2,3,4)
+            .reduce(0,(acc,element) -> acc + element);
+
+        List<Integer> together = Stream.of(asList(1,2,6,8), asList(3,4,8,7,33))
+                .flatMap(Collection::stream)
+                .collect(toList());
+    }
+
     }
 
 
